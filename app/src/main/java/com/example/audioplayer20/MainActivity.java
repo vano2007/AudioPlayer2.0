d56b252d887c491fc48ae78fd798416b913cef00
Eduard Angelskiy <ed_angel74@mail.ru>
2023-03-26 22:48:50 +0700

    Commit_260323

--- app/src/main/java/com/example/audioplayer20/MainActivity.java
+++ app/src/main/java/com/example/audioplayer20/MainActivity.java
@@ -6,6 +6,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // создание полей
+    private MediaPlayer mediaPlayer = new MediaPlayer(); // создание поля медиа-плеера
+    private SeekBar seekBar; // создание поля SeekBar
+    private boolean wasPlaying = false; // поле проигрывания аудио-файла
+    private FloatingActionButton fabPlayPause; // поле кнопки проигрывания и постановки на паузу аудиофайла
+    private TextView seekBarHint; // поле информации у SeekBar
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
